7faa3f8b52888bdf18e27b96caaa4f161a11bbc5
Qrowchen <38583235+Qrowchen@users.noreply.github.com>
2020-04-12 15:07:09 +0800

    Update network.py

--- network.py
+++ network.py
@@ -1,28 +1,38 @@
 import tensorflow as tf
 from tensorflow.keras import layers, optimizers, datasets, Sequential
 import os
-
+import pickle
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 conv_layers = [
     layers.Conv2D(128, kernel_size=[3, 3], padding="same", activation=tf.nn.relu),
+    layers.Conv2D(128, kernel_size=[3, 3], padding="same", activation=tf.nn.relu),
     layers.MaxPooling2D(pool_size=[2, 2], strides=2, padding='same'),
-    layers.Dropout(rate=0.5),
+    layers.Dropout(rate=0.6),
+
+    layers.Conv2D(128, kernel_size=[3, 3], padding="same", activation=tf.nn.relu),
+    layers.Conv2D(128, kernel_size=[3, 3], padding="same", activation=tf.nn.relu),
+    layers.MaxPooling2D(pool_size=[2, 2], strides=2, padding='same'),
+    layers.Dropout(rate=0.6),
 
     layers.Conv2D(256, kernel_size=[3, 3], padding="same", activation=tf.nn.relu),
+    layers.Conv2D(256, kernel_size=[3, 3], padding="same", activation=tf.nn.relu),
     layers.MaxPooling2D(pool_size=[2, 2], strides=2, padding='same'),
-    layers.Dropout(rate=0.5),
+    layers.Dropout(rate=0.6),
 
-    layers.Conv2D(512, kernel_size=[3, 3], padding="same", activation=tf.nn.relu),
+    layers.Conv2D(256, kernel_size=[3, 3], padding="same", activation=tf.nn.tanh),
+    layers.Conv2D(256, kernel_size=[3, 3], padding="same", activation=tf.nn.tanh),
     layers.MaxPooling2D(pool_size=[2, 2], strides=2, padding='same'),
-    layers.Dropout(rate=0.5),
+    layers.Dropout(rate=0.6),
 
-    layers.Conv2D(1024, kernel_size=[3, 3], padding="same", activation=tf.nn.relu),
+    layers.Conv2D(512, kernel_size=[3, 3], padding="same", activation=tf.nn.tanh),
+    layers.Conv2D(512, kernel_size=[3, 3], padding="same", activation=tf.nn.tanh),
     layers.MaxPooling2D(pool_size=[2, 2], strides=2, padding='same'),
+    layers.Flatten()
 ]
 
 fc_layers = Sequential([
-    layers.Dense(1024, activation=tf.nn.relu),
     layers.Dense(512, activation=tf.nn.relu),
-    layers.Dense(62*4, activation=None),
+    layers.Dense(256, activation=tf.nn.relu),
+    layers.Dense(62, activation=None),
 ])
