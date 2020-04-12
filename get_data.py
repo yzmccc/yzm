63a6e6b263cd4aceba24e28e6c0a308275b4668c
Qrowchen <38583235+Qrowchen@users.noreply.github.com>
2020-04-12 15:10:40 +0800

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,13 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import layers, optimizers, datasets, Sequential
-import os
-import matplotlib.image as mpimg  # mpimg 用于读取图片 x
-import pandas as pd  # 导入 y train
-import keras.backend as K
-import numpy as np
-import matplotlib.pyplot as plt
-import math
-import matplotlib.pyplot as plt  # plt 用于显示图片
+
 import matplotlib.image as mpimg  # mpimg 用于读取图片
 import os  # 用于从文件夹读取，计数文件（注意不一定是图片）
 import numpy as np
@@ -15,123 +8,83 @@ import scipy.misc
 import scipy.signal
 import scipy.ndimage
 import cv2
+import collections
+import pandas as pd
+from lead_in_csv import lead_in_csv
+from preprocess import *
+#实际图片读取路径 pic_train_orig_x
+pic_train_orig_x = "data/5000_origin"
+#实际csv读取路径 pic_train_orig_x
+pic_train_orig_y = "data/over.csv"
 
 
-pic_train_orig_x = "data/train"
-pic_train_orig_y = "data/train_label.csv"
-
-pic_wanna_orig = "data/test"
-# 独热eye元素个数
-eye_num = 62
-
-
-# y_array -> y_list
-def y_to_list(y_array):
-    y_list = y_array.tolist()
-    finall_box = []
-    mini_box = []
-    for i in y_list:
-        mini_box.append(i[0][0])
-        mini_box.append(i[0][1])
-        mini_box.append(i[0][2])
-        mini_box.append(i[0][3])
-        # print(mini_box)
-        finall_box.append(mini_box)
-        mini_box = []
-    return finall_box
-
-
-# 输出如 ：[['4', 'J', 'y', '3'], ['P', '6', 'B', 'f'], ['v', 'J', 'l', 'D'].......]]
-
-# 将数字字母转为62个数字
-def y_to_pure_num(y_solid_alphabet):
-    finall_num_y_pack = []
-    for i in y_solid_alphabet:
-        cache = []
-        for m in range(0, 4):
-            if i[m] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-                cache.append(int(i[m]))
-            elif i[m] in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-                          't', 'u', 'v', 'w', 'x', 'y', 'z']:
-                cache.append(int(ord(i[m]) - 87))  # a是ascii97号,10到35
-            elif i[m] in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-                          'T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
-                cache.append(int(ord(i[m]) - 29))  # A是ascii65号，36到61号
-        finall_num_y_pack.append(cache)
-    return finall_num_y_pack
-
-
-# 输出如 ：[[4, 44, 34, 3], .......]]
-
-
-def load_dataset():
-    filelist = os.listdir(pic_train_orig_x)
-    train_total_num = len(filelist)
+def load_image(number=None,read=None):
+    # 传如number限制时，一共就读numbeer个。
+    # read为true不净化只切割，否则调用cut_delnoise_image.preprocess先切后净一步到位
+    if number is not None and number != 'all':
+        train_total_num = number
+    # 否则，读取路径 pic_train_orig_x（= "data/del"）所有文件
+    else:
+        filelist = os.listdir(pic_train_orig_x)
+        train_total_num = len(filelist)
+        print(train_total_num)          # 打印读取对象数目 train_total_num
     train_set_x_orig_cache = []
     for pic_num in range(0, train_total_num):
-        X_train_orig = mpimg.imread(pic_train_orig_x + "/" + str(pic_num + 1) + '.jpg')
-        train_set_x_orig_cache.append(X_train_orig)
-    # x train 读取
+        if read == True:
+            #默认，循环读取jpg文件到 X_train_orig变量缓存
+            X_train_orig = mpimg.imread(pic_train_orig_x + "/" + str(pic_num + 1) + '.jpg')
+            #切割！！！！ 返回切割后list
+            cut_im = cut_image(X_train_orig)
+        else:
+            X_train_orig = mpimg.imread(pic_train_orig_x + "/" + str(pic_num + 1) + '.jpg')
+            cut_im = cut_delnoise_image(X_train_orig)
+        for i in range(len(cut_im)):
+            # train_set_x_orig_cache 缓存所有切割后的图片
+            train_set_x_orig_cache.append(cut_im[i])
+    # train_set_x_是array化后切割图像矩阵缓存
     train_set_x_ = np.array(train_set_x_orig_cache)
-    # y train 读取
-    train_set_y_orig_cache = pd.read_csv(pic_train_orig_y, header=None)
-    train_set_y_ = np.array(train_set_y_orig_cache)
-    # print(train_set_x_)
-
-    # train_set_x_  train_set_y_
-    # train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig,
-    #  分割为 6 : 4
-    train_set_x_orig = train_set_x_[:int(0.6 * train_total_num)]
-    test_set_x_orig = train_set_x_[int(0.6 * train_total_num): train_total_num]
-    train_set_y_orig = train_set_y_[:int(0.6 * train_total_num)]
-    test_set_y_orig = train_set_y_[int(0.6 * train_total_num): train_total_num]
-
+    # y train 读取,调用lead_in_csv文件的lead_in_csv函数
+    train_set_y_ = np.array(lead_in_csv())
+    #  因为每张图x切割为四，y也返回一个个字母，故总计算对象数乘四
+    train_total_num = train_total_num * 4
+    # 分割train/test取后0.8为训练集，前0.2算准确率当测试集
+    train_set_x_orig = train_set_x_[int(0.2* train_total_num): train_total_num]
+    test_set_x_orig = train_set_x_[0:int(0.2* train_total_num)]
+    train_set_y_orig = train_set_y_[int(0.2* train_total_num): train_total_num]
+    test_set_y_orig = train_set_y_[0:int(0.2* train_total_num)]
+    # 将y元素转换为数字
     y_train_num_list = y_to_pure_num(y_to_list(train_set_y_orig))
     y_test_num_list = y_to_pure_num(y_to_list(test_set_y_orig))
     print(y_train_num_list)
+    print(y_test_num_list)
     return train_set_x_orig, y_train_num_list, test_set_x_orig, y_test_num_list
 
 
-def preprocess(x, y):
-    # [0-1]
-    x = tf.cast(x, dtype=tf.float32) / 255.
-    y = tf.cast(y, dtype=tf.int32)
-    return x, y
-
-def convert_to_one_hot2(Y, C=62):
-    print("转换为独热")
-    temp=np.array(Y)
-    ##################### 对Y长度统一性进行检查
-    print("开始对Y长度统一性进行检查")
-    for i in range(0,len(Y)):
-        if len(Y[i]) != 4:
-            print("第"+ str(i+1) +"个错误")
-            print(Y[i])
-    print("this time ,Y-len:"+str(len(Y)))
-    print("结束对Y长度统一性进行检查")
-    #####################
-    Y = np.eye(C)[temp.reshape(len(Y),4)]
-    print("转换结束")
-    return Y
-
-
-def batch_set():
-
-    train_db, y, test_db, y_test = load_dataset()
-    y_onehot = convert_to_one_hot2(y)
-    print(y_onehot)
-    train_db = tf.convert_to_tensor(train_db,dtype=tf.int32)
-    y = tf.convert_to_tensor(y,dtype=tf.int32)
-    test_db=tf.convert_to_tensor(test_db,dtype=tf.int32)
-    y_test=tf.convert_to_tensor(y_test,dtype=tf.int32)
-
-    print(train_db.shape, y.shape)
-
-    train_db = train_db.shuffle(1000).map(preprocess).batch(64)
-
-    test_db = test_db.map(preprocess).batch(64)
+def load_datasets():
+    x, y, x_test, y_test = load_image(read=True)
+    # x,y 成对绑定
+    train_db = tf.data.Dataset.from_tensor_slices((x, y))
+    # 小组打乱
+    train_db = train_db.shuffle(1000).map(preprocess).batch(32)
+    test_db = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+    test_db = test_db.map(preprocess).batch(32)
     sample = next(iter(train_db))
-    print(sample[0].shape)
-    return train_db,y,test_db,y_test
+    # 打印一个batch中x，y形状
+    print(sample[0].shape, sample[1].shape)
+
+    return train_db, test_db
+
+
+#测试用
 if __name__ == '__main__':
-    batch_set()
+    x, y, x_test, y_test = load_image(read=True)
+
+# =============================================================================
+#
+# train_set_x_orig, y_train_num_list, test_set_x_orig, y_test_num_list=load_dataset()
+# for i in range(len(train_set_x_orig)):
+#     plt.imshow(train_set_x_orig[i])
+#     plt.title(y_train_num_list[i])
+#     plt.show()
+# =============================================================================
+
