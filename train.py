11b802450b87c54f6ff0046cf6eeeb126b7acb05
Qrowchen <38583235+Qrowchen@users.noreply.github.com>
2020-04-12 15:08:36 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,63 +1,73 @@
 import tensorflow as tf
 from tensorflow.keras import layers, optimizers, datasets, Sequential
-import os
-import network
-from get_data import batch_set
+from get_data import *
 from network import conv_layers, fc_layers
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# pic_train_orig_x = "data/train"     #
+pic_train_orig_y = "data/train_label.csv"   # csv读取路径，应该有所有的图的y
+# pic_wanna_orig = "data/test"                #
+pic_train_del = "data/del"                  # 图片读取路径
+
+# 输入 测试的 （x，y）/卷积网络层参数/全链接网络层参数/循环轮回数/？？？
+def predict(db, conv_net, fc_net, epoch, acclist):
+    total_num = 0
+    total_correct = 0
+    for x, y in db:
+        out = conv_net(x)
+        logits = fc_net(out)
+        prob = tf.nn.softmax(logits, axis=1)
+        pred = tf.argmax(prob, axis=1)                          #输出单个的自定义ascii的代码【1，3，5】
+        pred = tf.cast(pred, dtype=tf.int32)
+        correct = tf.cast(tf.equal(pred, y), dtype=tf.int32)    #对比，相同为1【1，1，0】
+        correct = tf.reduce_sum(correct)                        #计算多少个1，即多少个成功预测
+        total_num += x.shape[0]
+        total_correct += int(correct)
+    acc = total_correct / total_num
+    print(epoch, 'acc:', acc)                                   #epoch只为打印是第多少个轮回
+    acclist.append(acc)
+    print(acclist)                                              #打印到现在为止每次的准星列表
+    return acc
+
 
 def main():
-    train_db, y, test_db, y_test = batch_set()
-    sample = next(iter(train_db))
-    print(sample[0].shape,sample[1].shape)
+    #加载数据
+    #函数在 get_dataset.py
+    print("读取数据ing")
+    train_db, test_db = load_datasets()
+    # 实体化网络，具体在network.py
+    print("加载网络ing")
     conv_net = Sequential(conv_layers)
-    conv_net.build(input_shape=[None, 40, 120, 3])
-    fc_net=Sequential(fc_layers)
+    conv_net.build(input_shape=[None, 40, 25, 3])
+    fc_net = Sequential(fc_layers)
     fc_net.build(input_shape=[None, 1024])
-    optimizer = optimizers.Adam(lr=1e-4)
-    # [1,2]+[3,4] = [1,2,3,4]
+    optimizer = optimizers.Adam(lr=1e-5)
     variables = conv_net.trainable_variables + fc_net.trainable_variables
-
-    for epoch in range(3):
+    # acctrain存储之前每次准星，acctest相似
+    print("开始训练")
+    acctrain = []
+    acctest = []
+    for epoch in range(200):
         for step, (x, y) in enumerate(train_db):
             with tf.GradientTape() as tape:
-                # [b,120,40,3]
                 out = conv_net(x)
-                # flatten ==> [b,512]
-                out = tf.reshape(out, [-1, 512])
-                # [b,512] --> [b,100]
                 logits = fc_net(out)
-                # [b] --> [b,100]
-                y_onehot = tf.one_hot(y, depth=100)
-                # compute loss
+                y_onehot = tf.one_hot(y, depth=62)
                 loss = tf.losses.categorical_crossentropy(y_onehot, logits, from_logits=True)
                 loss = tf.reduce_mean(loss)
-
             grads = tape.gradient(loss, variables)
             optimizer.apply_gradients(zip(grads, variables))
-
             if step % 100 == 0:
                 print(epoch, step, 'loss:', float(loss))
 
-        total_num = 0
-        total_correct = 0
-        for x, y in test_db:
-            out = conv_net(x)
-            out = tf.reshape(out, [-1, 1024])
-            logits = fc_net(out)
-            prob = tf.nn.softmax(logits, axis=1)
-            pred = tf.argmax(prob, axis=1)
-            pred = tf.cast(pred, dtype=tf.int32)
+        print('train:')
+        new_train_acc = predict(train_db, conv_net, fc_net, epoch, acctrain)
 
-            correct = tf.cast(tf.equal(pred, y), dtype=tf.int32)
-            correct = tf.reduce_sum(correct)
-
-            total_num += x.shape[0]
-            total_correct += int(correct)
-
-        acc = total_correct / total_num
-        print(epoch, 'acc:', acc)
+        print('test:')
+        new_test_acc = predict(test_db, conv_net, fc_net, epoch, acctest)
+        # 如果test准星最高，存储一次 h5 文件
+        if new_test_acc>max(acctest):
+            conv_net.save("model/conv_model.h5")
+            fc_net.save("model/fc_model.h5")
 
 
 if __name__ == '__main__':
